aab3d958172992c690b51d26b8e3caeda176e3ca
Ender36046 <117245335+Ender36046@users.noreply.github.com>
2026-09-05 21:26:06 -0600

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        memo[0] = 1
+        for i in range(1, n+1):
+            memo[i] = 0
+            for step in [1,2]:
+                subProblem = i - step
+                if subProblem < 0: continue
+
+                memo[i] = memo[i] + memo[subProblem]
+        return memo[n]
