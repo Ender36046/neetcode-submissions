9021477d3bb01c0e44b859fb0961a2286dc7b25e
Ender36046 <117245335+Ender36046@users.noreply.github.com>
2026-09-05 22:38:33 -0600

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,18 @@
+"""
+
+arr
+
+"""
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if(len(nums) < 3):
+            return max(nums)
+        arr = [0] * len(nums)
+        arr[0] = nums[0]
+        arr[1] = max(nums[0:2])
+        for i in range(2, len(nums)):
+            arr[i] = max(arr[i-2] + nums[i], arr[i-1])
+        return arr[len(nums)-1]
+        
